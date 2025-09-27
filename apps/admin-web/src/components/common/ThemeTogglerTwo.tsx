3cf267a0b0bbaf8f046b53d234dfd8750cedae6e
Hieu <nguyentrunghieu170221@gmail.com>
2025-09-27 16:29:53 +0700

    [Role Admin] Màn hình đăng nhập

--- apps/admin-web/src/components/common/ThemeTogglerTwo.tsx
+++ apps/admin-web/src/components/common/ThemeTogglerTwo.tsx
@@ -5,8 +5,7 @@ export default function ThemeTogglerTwo() {
   return (
     <button
       onClick={toggleTheme}
-      className="inline-flex items-center justify-center text-white transition-colors rounded-full size-14 bg-brand-500 hover:bg-brand-600"
-    >
+      className="inline-flex items-center justify-center text-white transition-colors rounded-full size-14 bg-[#ff5e00ff] hover:bg-[#e55600ff] focus:outline-none focus:ring-2 focus:ring-[#ffb38c]">
       <svg
         className="hidden dark:block"
         width="20"
--- apps/admin-web/src/components/ui/button/Button.tsx
+++ apps/admin-web/src/components/ui/button/Button.tsx
@@ -1,9 +1,9 @@
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 interface ButtonProps {
   children: ReactNode; // Button text or content
   size?: "sm" | "md"; // Button size
-  variant?: "primary" | "outline"; // Button variant
+  variant?: "primary" | "outline" | "orange" ; // Button variant
   startIcon?: ReactNode; // Icon before the text
   endIcon?: ReactNode; // Icon after the text
   onClick?: () => void; // Click handler
@@ -33,7 +33,9 @@ const Button: React.FC<ButtonProps> = ({
       "bg-brand-500 text-white shadow-theme-xs hover:bg-brand-600 disabled:bg-brand-300",
     outline:
       "bg-white text-gray-700 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:bg-gray-800 dark:text-gray-400 dark:ring-gray-700 dark:hover:bg-white/[0.03] dark:hover:text-gray-300",
-  };
+    orange:
+      "bg-[#ff5e00ff] text-white shadow-theme-xs hover:bg-[#e55600ff] disabled:bg-[#ffb38cff]"
+    };
 
   return (
     <button
