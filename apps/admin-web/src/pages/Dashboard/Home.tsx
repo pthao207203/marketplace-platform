bad77d1951a82dbf24d69f69290e68c825c0dedc
Hieu <nguyentrunghieu170221@gmail.com>
2025-09-27 16:29:53 +0700

    [Role Admin] Màn hình đăng nhập

--- apps/admin-web/src/pages/Dashboard/Home.tsx
+++ apps/admin-web/src/pages/Dashboard/Home.tsx
@@ -1,11 +1,7 @@
 import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
-import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
-import StatisticsChart from "../../components/ecommerce/StatisticsChart";
+import SalesChart from "../../components/ecommerce/SalesChart";
 import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
-import RecentOrders from "../../components/ecommerce/RecentOrders";
-import DemographicCard from "../../components/ecommerce/DemographicCard";
 import PageMeta from "../../components/common/PageMeta";
-
 export default function Home() {
   return (
     <>
@@ -16,8 +12,6 @@ export default function Home() {
       <div className="grid grid-cols-12 gap-4 md:gap-6">
         <div className="col-span-12 space-y-6 xl:col-span-7">
           <EcommerceMetrics />
-
-          <MonthlySalesChart />
         </div>
 
         <div className="col-span-12 xl:col-span-5">
@@ -25,15 +19,7 @@ export default function Home() {
         </div>
 
         <div className="col-span-12">
-          <StatisticsChart />
-        </div>
-
-        <div className="col-span-12 xl:col-span-5">
-          <DemographicCard />
-        </div>
-
-        <div className="col-span-12 xl:col-span-7">
-          <RecentOrders />
+          <SalesChart />
         </div>
       </div>
     </>
